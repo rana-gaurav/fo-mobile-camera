907a4b78a7ec5ccb32a5b3e13dbf111c3a66d991
ajayvamsee <ajayvamsee329@gmail.com>
2021-08-18 11:36:41 +0530

    Camera Data colletion ui screen

--- TestApp/app/src/main/java/com/faceopen/camerabenchmark/cameraoptions/CameraOpActivity2.java
+++ TestApp/app/src/main/java/com/faceopen/camerabenchmark/cameraoptions/CameraOpActivity2.java
@@ -1,14 +1,15 @@
 package com.faceopen.camerabenchmark.cameraoptions;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.faceopen.camerabenchmark.CameraActivity;
 import com.faceopen.camerabenchmark.R;
 import com.faceopen.camerabenchmark.adapter.CustomAdapter1;
 
@@ -16,17 +17,21 @@ public class CameraOpActivity2 extends AppCompatActivity {
 
     GridView gridView;
     String[] items = {"Under Light", "Under SunLight", "Left Side Light", "Right Side", "Light on Straight Face"};
-    Integer[] logo = {R.drawable.ic_android, R.drawable.ic_android, R.drawable.ic_android, R.drawable.ic_android, R.drawable.ic_android};
+    Integer[] logo = {R.drawable.ic_android,
+            R.drawable.ic_android,
+            R.drawable.ic_android,
+            R.drawable.ic_android,
+            R.drawable.ic_android};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_op2);
 
-        gridView=findViewById(R.id.gridView);
+        gridView = findViewById(R.id.gridView);
 
 
-        CustomAdapter1 adapter1=new CustomAdapter1(this,items,logo);
+        CustomAdapter1 adapter1 = new CustomAdapter1(this, items, logo);
         gridView.setAdapter(adapter1);
 
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -34,10 +39,11 @@ public class CameraOpActivity2 extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
 
-                String itemValue=(String)   gridView.getItemAtPosition(position);
+                String itemValue = (String) gridView.getItemAtPosition(position);
+                Toast.makeText(CameraOpActivity2.this, itemValue, Toast.LENGTH_SHORT).show();
 
-                    Toast.makeText(CameraOpActivity2.this,itemValue, Toast.LENGTH_SHORT).show();
-
+                Intent intent = new Intent(CameraOpActivity2.this, CameraActivity.class);
+                startActivity(intent);
 
 
             }
